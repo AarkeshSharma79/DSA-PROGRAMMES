1dd5c1d816a9e72ae7598de117611c9da79f03d6
Aarkesh sharma <aarkeshsharma17@gmail.com>
2026-02-05 22:31:26 +0530

    Time: 3 ms (19.69%), Space: 8 MB (55.58%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPrime(int n)
+    {
+        if(n==1) return false;
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    int smallestValue(int n) {
+        int sum=0;
+        if(isPrime(n)) return n;
+        else
+        {
+            for(int i=1;i<sqrt(n);i++)
+            {
+                if(isPrime(i)&& n%i==0){
+                    int m=n;
+                    while(m%i==0)
+                    {
+                        sum+=i;
+                        m/=i;
+                    }
+                }
+            }
+            for(int i=sqrt(n);i>=1;i--)
+            {
+                if(isPrime(n/i)&&n%i==0){
+                    int m=n;
+                    while(m%(n/i)==0)
+                    {
+                        sum+=(n/i);
+                        m/=(n/i);
+                    }
+                }
+            }
+        }
+        if(sum==n) return n;
+        return smallestValue(sum);
+    }
+};
